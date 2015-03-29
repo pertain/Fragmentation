947f453ffad2772f73dcb9bcf436698dff7d9d1e
william <4yellowpeppers@gmail.com>
2015-03-29 08:28:06 -0400

    syntax changes - still needs refactoring

--- dasSkript.sh
+++ dasSkript.sh
@@ -19,6 +19,10 @@
 # and read speed of the flash drive (both before and after
 # it has been fragmented).
 
+# TO DO:
+#   1.  modularize w/ functions
+#   2.  change echo calls to printf
+
 
 # ======================================================
 # Prep for the experiment
@@ -35,11 +39,11 @@ j=1
 while [ $i -lt 9000 ]
 do
     nine_kb+="."
-    if [ $i -eq 1000 ]
+    if [ "$i" -eq 1000 ]
     then
-        one_kb=$nine_kb
+        one_kb="$nine_kb"
     fi
-    i=$[$i+1]
+    i="$[$i+1]"
 done
 echo
 
@@ -47,17 +51,17 @@ echo "Formatting flash drive to ext4..."
 echo
 
 # Unmount the flash drive
-sudo umount $MOUNT_POINT
+sudo umount "$MOUNT_POINT"
 sleep 1
 
 # Format the flash drive to ext4
-sudo mkfs.ext4 $DEVICE
+sudo mkfs.ext4 "$DEVICE"
 echo
 sleep 1
 
 # Mount the flash drive
-sudo mount $DEVICE $MOUNT_POINT
-sudo chown william:william $MOUNT_POINT
+sudo mount "$DEVICE" "$MOUNT_POINT"
+sudo chown william:william "$MOUNT_POINT"
 sleep 1
 
 
@@ -67,14 +71,14 @@ sleep 1
 
 # Copying huge.txt to freshly formatted flash drive
 # (used for pre-fragmentation read test)
-cp ./huge.txt $MOUNT_POINT"/"
+cp ./huge.txt "$MOUNT_POINT"/
 
 # Pause and wait for user input
 read -p "Press [Enter] to test fragmentation status..."
 echo
 
 # Check fragmentation status
-sudo e2fsck -fn $DEVICE
+sudo e2fsck -fn "$DEVICE"
 echo
 
 # Pause and wait for user input
@@ -86,11 +90,11 @@ sudo /sbin/sysctl vm.drop_caches=3
 sleep 1
 
 # Run read speed test
-time dd if=$MOUNT_POINT/huge.txt of=/dev/null bs=1K
+time dd if="$MOUNT_POINT"/huge.txt of=/dev/null bs=1K
 sleep 1
 
 # Remove huge.txt
-rm $MOUNT_POINT/huge.txt
+rm "$MOUNT_POINT"/huge.txt
 echo
 
 # Pause and wait for user input
@@ -103,16 +107,16 @@ echo
 # ======================================================
 
 # Writes alternating 9Kb and 1Kb files
-echo "Writing files to "$MOUNT_POINT
-while [ $j -le 10785 ]
+echo "Writing files to ""$MOUNT_POINT"
+while [ "$j" -le 10785 ]
 do
     if [ `echo "$j % 719" | bc` -eq 0 ]
     then
         echo -n -e "."
     fi
-    echo $nine_kb > $MOUNT_POINT"/large_"$j".txt"
-    echo $one_kb > $MOUNT_POINT"/small_"$j".txt"
-    j=$[$j+1]
+    echo "$nine_kb" > "$MOUNT_POINT"/large_"$j".txt
+    echo "$one_kb" > "$MOUNT_POINT"/small_"$j".txt
+    j="$[$j+1]"
 done
 echo -n -e " Done!"
 echo
@@ -120,14 +124,14 @@ sleep 1
 
 # Deletes all of the 1Kb files
 echo "Deleting small files"
-rm $MOUNT_POINT/*small*.txt
+rm "$MOUNT_POINT"/*small*.txt
 
 # A brief pause to ensure all files delete properly
 sleep 2
 
 # Copies huge.txt to flash drive
 echo "Copying huge.txt (should fragment on write)"
-cp ./huge.txt $MOUNT_POINT"/"
+cp ./huge.txt "$MOUNT_POINT"/
 echo
 
 
@@ -140,7 +144,7 @@ read -p "Press [Enter] to test fragmentation status..."
 echo
 
 # Check fragmentation status
-sudo e2fsck -fn $DEVICE
+sudo e2fsck -fn "$DEVICE"
 echo
 
 # Pause and wait for user input
@@ -152,5 +156,5 @@ sudo /sbin/sysctl vm.drop_caches=3
 sleep 1
 
 # Run read speed test
-time dd if=$MOUNT_POINT/huge.txt of=/dev/null bs=1K
+time dd if="$MOUNT_POINT"/huge.txt of=/dev/null bs=1K
 echo
